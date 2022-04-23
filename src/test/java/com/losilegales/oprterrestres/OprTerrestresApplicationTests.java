a591ef86d01532179f46880c7b888c8392f282ec
gfgustavo2015 <gfgustavo2015@gmail.com>
2022-04-23 20:10:48 -0300

    feat: first commit opr-terrestres

---
+++ src/test/java/com/losilegales/oprterrestres/OprTerrestresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.losilegales.oprterrestres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OprTerrestresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
