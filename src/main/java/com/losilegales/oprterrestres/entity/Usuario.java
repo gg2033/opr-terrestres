1ef1f44b62e78e8e0112a26f36dd5bcbaedae291
gfgustavo2015 <gfgustavo2015@gmail.com>
2022-05-01 13:33:08 -0300

    fix: enviroment variable v3

--- src/main/java/com/losilegales/oprterrestres/entity/Usuario.java
+++ src/main/java/com/losilegales/oprterrestres/entity/Usuario.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
-@Table(name="USUARIO",schema="public")
+@Table(name="usuarios",schema="public")
 public class Usuario implements Serializable {
 	
     /**
