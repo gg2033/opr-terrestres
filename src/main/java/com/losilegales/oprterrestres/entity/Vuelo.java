04a2616d61143bbdda9080430a1b3e7adfb17563
gfgustavo2015 <gfgustavo2015@gmail.com>
2022-05-02 20:56:49 -0300

    fix: correction entityes, because restructure in tables

--- src/main/java/com/losilegales/oprterrestres/entity/Vuelo.java
+++ src/main/java/com/losilegales/oprterrestres/entity/Vuelo.java
@@ -1,23 +1,21 @@
 package com.losilegales.oprterrestres.entity;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.time.LocalDateTime;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
 @Entity
-@Table(name="vuelo",schema="public")
-//@Data
+@Table(name="vuelos",schema="public")
+@Data
 public class Vuelo {
 	
 	@Id
@@ -25,11 +23,28 @@ public class Vuelo {
 	@Column(name="id_vuelo")
 	private Integer idVuelo;
 	
-	@Column(name="codigo_vuelo")
-	private String codigoVuelo;
+	@ManyToOne
+	@JoinColumn(name="id_plan_de_ruta")
+	private PlanRuta planRuta;
 	
-	@Column(name="fecha")
-	private LocalDate fechaVuelo;
+	@Column(name="id_aerolinea")
+	private Integer idAerolinea;
+	
+	@Column(name="codigo")
+	private String codigoVuelo;
+	@Column(name="horario_salida")
+	private LocalDateTime horarioSalida;
+	@Column(name="horario_llegada")
+	private LocalDateTime horarioLlegada;
+	
+//	@OneToOne
+//	private AeroPuerto aeropuertoSalida;
+	
+//	@OneToOne
+//	private AeroPuerto aeropuertoLlegada;
+	
+//	@Column(name="fecha")
+//	private LocalDate fechaVuelo;
 	
 	@Column(name="cantidad_pasajeros")
 	private Integer cantPasajeros;
@@ -37,49 +52,9 @@ public class Vuelo {
 //	@OneToMany(mappedBy = "vuelo")
 //	private List<Tripulante> tripulantes;
 	
-	@OneToMany(mappedBy = "vuelo", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Pasajero> pasajeros;
+//	@OneToMany(mappedBy = "vuelo", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+//	private List<Pasajero> pasajeros;
 
-	public Integer getIdVuelo() {
-		return idVuelo;
-	}
-
-	public void setIdVuelo(Integer idVuelo) {
-		this.idVuelo = idVuelo;
-	}
-
-	public String getCodigoVuelo() {
-		return codigoVuelo;
-	}
-
-	public void setCodigoVuelo(String codigoVuelo) {
-		this.codigoVuelo = codigoVuelo;
-	}
-
-	public LocalDate getFechaVuelo() {
-		return fechaVuelo;
-	}
-
-	public void setFechaVuelo(LocalDate fechaVuelo) {
-		this.fechaVuelo = fechaVuelo;
-	}
-
-	public Integer getCantPasajeros() {
-		return cantPasajeros;
-	}
-
-	public void setCantPasajeros(Integer cantPasajeros) {
-		this.cantPasajeros = cantPasajeros;
-	}
-
-	public List<Pasajero> getPasajeros() {
-		return pasajeros;
-	}
-
-	public void setPasajeros(List<Pasajero> pasajeros) {
-		this.pasajeros = pasajeros;
-	}
-	
 //	@Column(name="id_aeronave")
 //	private Integer idAeronave;
 	
