5e9da67776432ad38bc0989646938e0cfc31d533
diego_lopez <dl3464452@gmail.com>
2022-05-03 15:03:29 -0300

    Agregados algunos mensajes para las excepciones mas comunes.

---
+++ src/main/java/com/losilegales/oprterrestres/exceptions/UsuarioServiceException.java
@@ -0,0 +1,12 @@
+package com.losilegales.oprterrestres.exceptions;
+
+public class UsuarioServiceException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8518006985660622198L;
+
+	public UsuarioServiceException(String message) {
+		super(message);
+	}
+}
