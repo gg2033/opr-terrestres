ecab7c2d97b24c0a58478ceef885a94c9018c1a7
diego_lopez <dl3464452@gmail.com>
2022-05-07 17:15:35 -0300

    Agregado las excepciones, controller y repository de las entidades permisos y permisos de rol junto con sus enpoints para GET.

---
+++ src/main/java/com/losilegales/oprterrestres/exceptions/PermisoServiceException.java
@@ -0,0 +1,13 @@
+package com.losilegales.oprterrestres.exceptions;
+
+public class PermisoServiceException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5239144616573203254L;
+
+	public PermisoServiceException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/losilegales/oprterrestres/exceptions/RolPermisoServiceException.java
@@ -0,0 +1,14 @@
+package com.losilegales.oprterrestres.exceptions;
+
+public class RolPermisoServiceException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6274113932991275301L;
+
+	public RolPermisoServiceException(String message) {
+		super(message);
+	}
+
+}
