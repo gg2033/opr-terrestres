b62f6cc119726117de9f9a800016e9a16039f957
gfgustavo2015 <gfgustavo2015@gmail.com>
2022-05-06 21:13:35 -0300

    feat: card 008 fix

--- src/main/java/com/losilegales/oprterrestres/utils/OprConstants.java
+++ src/main/java/com/losilegales/oprterrestres/utils/OprConstants.java
@@ -8,6 +8,8 @@ public final class OprConstants {
 	public static final String MAESTRO_AERONAVES_V2 = "https://query.wikidata.org/sparql?query=SELECT%20DISTINCT%20%3Fitem%20%3FitemLabel%20WHERE%20%7B%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22es%22.%20%7D%0A%20%20%7B%0A%20%20%20%20SELECT%20DISTINCT%20%3Fitem%20WHERE%20%7B%0A%20%20%20%20%20%20%3Fitem%20p%3AP31%20%3Fstatement0.%0A%20%20%20%20%20%20%3Fstatement0%20(ps%3AP31%2F(wdt%3AP279*))%20wd%3AQ15056993.%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D&format=json";
 	public static final String CHECK_IN = "1ao7d3MhiUghl1AWw74FltDkA7dzcPnMXELeOLITDiJ0";
 	
+	public static final String CHECK_IN_JSON = "https://docs.google.com/spreadsheets/d/1Dx34WYiRO7-bLQrkX339jPKP6PtYY8-2rTMxVQIBfGU/gviz/tq?tqx=out:json&gid=0";
+	public static final String ENDPOINT_AERONAVES_JSON = "https://docs.google.com/spreadsheets/d/10KS3ai62T5_Y71pdkibZYHX9Vo7JKcw8IUqEymvGogg/gviz/tq?tqx=out:json&gid=0";
 	public static final String BASE_ENDPOINT = "/v1/losilegales";
 	
 	//Urls
