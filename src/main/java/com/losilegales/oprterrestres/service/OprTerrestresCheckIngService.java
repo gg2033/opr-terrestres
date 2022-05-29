26cfbc1d1fc19380841718fc44fa78f232a0216c
diego_lopez <dl3464452@gmail.com>
2022-05-29 01:03:54 -0300

    Arreglados unos bugs

--- src/main/java/com/losilegales/oprterrestres/service/OprTerrestresCheckIngService.java
+++ src/main/java/com/losilegales/oprterrestres/service/OprTerrestresCheckIngService.java
@@ -409,7 +409,7 @@ public class OprTerrestresCheckIngService {
 	
 	private void persistirDatosCheckin(ExcelResponse checkinData) {
 		//19 columnas tiene el checkin
-		checkinRepository.truncateTabla();
+//		checkinRepository.truncateTabla();
 		List<String> nombreColumn = new ArrayList<String>(19);
 		//Recorro el excel fila por columna y obtengo los nombres de las columnas
 		for (Col c : checkinData.getTable().getCols()) {
@@ -435,7 +435,7 @@ public class OprTerrestresCheckIngService {
 	
 	private void persistirDatosCarga(ExcelResponse cargaData) {
 		//19 columnas tiene el checkin
-		cargaRepository.truncateTabla();
+//		cargaRepository.truncateTabla();
 		List<String> nombreColumn = new ArrayList<String>(6);
 		//Recorro el excel fila por columna y obtengo los nombres de las columnas
 		for (Col c : cargaData.getTable().getCols()) {
