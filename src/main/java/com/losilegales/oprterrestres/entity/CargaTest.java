1263b8e9bd7731f7b3395b5a796555ddc7a076f8
diego_lopez <dl3464452@gmail.com>
2022-06-10 13:51:47 -0300

    Agregadas las entidades para el testeo

---
+++ src/main/java/com/losilegales/oprterrestres/entity/CargaTest.java
@@ -0,0 +1,67 @@
+package com.losilegales.oprterrestres.entity;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+
+@Entity
+@Table(name = "CARGAS_TEST", schema = "public")
+@Data
+public class CargaTest {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_carga")
+	private int idCarga;
+	
+	@Column(name = "codigo")
+	private String codigo;
+
+	@Column(name = "envio")
+	private int envio;
+	
+	@Column(name = "codigo_carga")
+	private String codigoCarga;
+	
+	@Column(name = "codigo_pasajero")
+	private String codigoPasajero;
+	
+	@Column(name = "tipo")
+	private String tipo;
+	
+	@Column(name = "peso_kg")
+	private int peso;
+
+	@Column(name = "tag")
+	private String tag;
+	
+	@Column(name="creado")
+	private LocalDate fechaCreacion;
+	
+	@Column(name="creado_por")
+	private String nombreCreador;
+	
+	@Column(name="modificado", nullable = true)
+	private LocalDate fechaModificacion;
+
+	@Column(name="modificado_por", nullable = true)
+	private String nombreModificador;
+
+	@Column(name="activo")
+	private boolean activo;
+	
+	@Column(name = "estado_carga")
+	private String estadoCarga;
+	
+	@Column(name = "codigo_vuelo")
+	private String codigoVuelo;
+}
---
+++ src/main/java/com/losilegales/oprterrestres/entity/CheckinTest.java
@@ -0,0 +1,98 @@
+package com.losilegales.oprterrestres.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="CHECKIN_TEST",schema="public")
+public class CheckinTest {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_checkin")
+	private int idCheckin;
+	
+	@Column(name = "codigo")
+	private String codigo;
+	
+	@Column(name="codigo_pasajero")
+	private String codigoPasajero;
+	
+	@Column(name="compania")
+	private String compania;
+	
+	@Column(name="codigo_vuelo")
+	private String codigoVuelo;
+	
+	@Column(name="fecha_partida")
+	private LocalDate fechaPartida;
+	
+	@Column(name="origen")
+	private String origen;
+	
+	@Column(name="destino")
+	private String destino;
+	
+	@Column(name="asiento")
+	private String asiento;
+	
+	@Column(name="clase")
+	private String clase;
+	
+	@Column(name="tipo_documento")
+	private String tipoDocumento;
+	
+	@Column(name="numero_documento")
+	private int numeroDocumento;
+	
+	@Column(name="apellido")
+	private String apellido;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="nacionalidad")
+	private String nacionalidad;
+	
+	@Column(name="edad")
+	private int edad;
+	
+	@Column(name="sexo")
+	private char sexo;
+	
+	@Column(name="alimentacion")
+	private String alimentacion;
+	
+	@Column(name="condicion")
+	private String condicion;
+	
+	@Column(name="comentario")
+	private String comentario;
+	
+	@Column(name="creado")
+	private LocalDate creado;
+	
+	@Column(name="creado_por")
+	private String creadoPor;
+	
+	@Column(name="modificado")
+	private LocalDate modificado;
+	
+	@Column(name="modificado_por")
+	private String modificadoPor;
+	
+	@Column(name="activo")
+	private boolean activo;
+}
